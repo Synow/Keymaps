eb6123ea04cc37ac90c403747879a3b0c79d1757
Synow <sivanhelpher@gmail.com>
2022-05-14 16:56:18 +0300

    Chocolad keyboard added

---
+++ jmnw/chocolad_rgb/keymaps/default/config.h
@@ -0,0 +1,20 @@
+#pragma once
+// Configure the OLED screen to reduce burn-in
+//#define OLED_TIMEOUT 10000
+//#define OLED_BRIGHTNESS 80
+//#define OLED_ENABLE
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 190
+
---
+++ jmnw/chocolad_rgb/keymaps/default/rules.mk
@@ -0,0 +1,12 @@
+LTO_ENABLE = yes
+SPLIT_KEYBOARD = yes
+EXTRAKEY_ENABLE = yes
+OLED_DRIVER_ENABLE = no
+RGBLIGHT_ENABLE = yes
+COMBO_ENABLE = no
+# Load the necessary support files
+# ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
+# 	SRC += oled.c
+# endif
+MOUSEKEY_ENABLE = yes       # Mouse keys
+NKRO_ENABLE = yes           # Enable N-Key Rollover
